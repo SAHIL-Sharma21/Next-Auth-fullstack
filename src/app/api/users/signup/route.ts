30e192da010d626d42d1873794a376e8915eb6bb
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-05-09 13:48:53 +0530

    mailsetup signup route complete

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,8 +1,10 @@
 //importing db connect method as in next js we have to conect to db in every routes its works different then core bwckend
 import {connectDB} from '@/dbConfig/dbConfig'
 import User from '@/models/userModel'//importing user from model
+import mongoose from 'mongoose';
 import {NextRequest, NextResponse} from 'next/server' // for making request in next js we import two method nextResponse and nextRequest
-
+import bcryptjs from 'bcryptjs';
+import {sendEmail} from '@/helpers/mailHelper'
 
 //sbse phele datavbase hi connect krne hai
 connectDB();
@@ -15,11 +17,47 @@ export async function POST(request:NextRequest) {
     try {
         
         //in nextjs we get the data by  request in next
-        const reqBody = request.json();// this reqBody is same as in express and we can destructure the value from it
+        //as in nextjs request,json is promiose and can take time to comes the data so we are using await.
+        const reqBody = await request.json();// this reqBody is same as in express and we can destructure the value from it
         const {username, email, password} = reqBody;
         //validation we will do in future using different libraries like yup, zod etc 
         console.log(reqBody);
         
+        //checking if user is present or not
+        const user = await User.findOne({email});
+
+        //if user hai toh nextresponse rerturn kr rahe hia
+        if(user) {
+            return NextResponse.json({error: "user already exists"}, {status: 400});
+        }
+
+        //if user is not present then we will create the user and hash it password before saving to DB
+        //generating salt and hasing the user paswword
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        //we are just creating model not interacting with database.
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        //now saving to db
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+        
+        //if user is saved then we will send verfication mail to user
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id});
+
+        //if email is sent the we will return the response
+        return NextResponse.json({
+            message: "User Registerd successfully",
+            success: true,
+            savedUser
+        })
+
+
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 500});
     }
--- src/helpers/mailHelper.ts
+++ src/helpers/mailHelper.ts
@@ -1,26 +1,59 @@
 //helper is same as utlils folder and here we will write functionality of mail
 
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs'
+
+
 
 export const sendEmail = async ({email, emailType, userId}: any) => {
     try {
-        //TODO: configure mail for usage
+       
+      //we will generate the hashed token by bcrypt
+      //hasing on the basis of userId 
+       const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+        if(emailType === "VERIFY"){
+            //if emailType is verify then we will find the user and save the verify token in database
+            await User.findByIdAndUpdate(userId, {
+              verifyToken: hashedToken,
+              verifyTokenExpiry: Date.now() + 3600000, //expires in 1 hour fromm the date which is saved.
+            });
+        } else if(emailType === "RESET"){
+            //if we want to reset the password then we will do the same task but for forgotPassword Token
+            await User.findByIdAndUpdate(userId, {
+              forgotPasswordToken: hashedToken,
+              forgotPasswordTokenExpiry: Date.now() + 3600000, //expires in 1 hour fromm the date which is saved.
+            });
+        }
+
+
         //using nodemailer service and making method by using documentation
         const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+          host: "sandbox.smtp.mailtrap.io",
+          port: 2525,
+          auth: {
+            user: process.env.MAILTRAP_USER,
+            pass: process.env.MAILTRAP_PASSWORD
+          }
+        });
+
+        const verifyMailBody = `<p>Click <a href="${process.env.DOMAIN}/verify-email?token=${hashedToken}">Here</a> to
+                              ${emailType === "VERIFY" ? "Verify your email" : "Reset your password"}
+                              or copy and paste the link below in your browser .
+                              <br> ${process.env.DOMAIN}/verify-email?token=${hashedToken}
+                              </p>` ;// we are genrating link and custom html for our mail.;
+        const resetMailBody = `<p>Click <a href="${process.env.DOMAIN}/reset-password?token=${hashedToken}">Here</a> to
+                              ${emailType === "VERIFY" ? "Verify your email" : "Reset your password"}
+                              or copy and paste the link below in your browser .
+                              <br> ${process.env.DOMAIN}/reset-password?token=${hashedToken}
+                              </p>`;
 
           const mailOptions = {
             from: 'sahil@dev.com', // sender address -- not the real one
             to: email, // list of receivers
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password", 
-            html: "<b>Hello world?</b>", // html body
+            html: emailType === "VERIFY" ? verifyMailBody : resetMailBody,// we are genrating link and custom html for our mail.
           }
 
           //sending email
